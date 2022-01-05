0274e95f596a3d42463c897e9ead5263d8d0078c
jgersain <jhersein@gmail.com>
2022-01-04 19:31:34 -0600

    Add scaffold for category and task

---
+++ app/models/task.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  due_date    :date
+#  category_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Task < ApplicationRecord
+  belongs_to :category
+end
